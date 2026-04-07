7f6848eeeb6b7110716909744e5fe7faa3f6ffc1
chamika <chamika@softvilmedia.com>
2026-04-07 17:27:29 +0530

    di container added

---
+++ src/di/container.py
@@ -0,0 +1,34 @@
+from dependency_injector import containers, providers
+from core.util import LoggerFactory, ILogger
+from core.data.repository.local_file_repository import LocalFileRepository
+from core.domain.usecases.data_io_usecase import LoadDataUseCase, SaveDataUseCase
+from etl.domain.usecases.data_cleaning_usecase import DataCleaningUseCase
+
+class AppContainer(containers.DeclarativeContainer):
+    """
+    DI for the app
+    """
+    config = providers.Configuration()
+
+    logger = providers.Singleton(
+        LoggerFactory.create,
+        logger_type="console",  # config.logger_type
+        name="App"
+    )
+
+    data_repository = providers.Singleton(
+        LocalFileRepository,
+        logger=logger
+    )
+
+    load_data_usecase = providers.Factory(
+        LoadDataUseCase,
+        repository=data_repository,
+        logger=logger
+    )
+
+    save_data_usecase = providers.Factory(
+        SaveDataUseCase,
+        repository=data_repository,
+        logger=logger
+    )
